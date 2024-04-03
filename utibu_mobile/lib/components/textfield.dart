26def7c2725b9b5b22ab2446a8b7d9eb00450229
IWANDETO <wandettoian@gmail.com>
2024-04-03 18:12:04 +0300

    Added Email and Password Inputs

---
+++ utibu_mobile/lib/components/textfield.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        controller: controller, // Gives Access what the User Types in from the Input Box
+        obscureText: obscureText, // Boolean to Hyde Charcters when Typing in User Passwod
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Color.fromARGB(255, 82, 173, 248))),
+          hintText: hintText, // String to Hint to User What should be typed in
+        ),
+      ),
+    );
+  }
+}
---
+++ utibu_mobile/lib/pages/login.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:utibu_mobile/components/textfield.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  // Controllers for Text Editing
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: const [
+                const SizedBox(height: 50),
+
+                //logo icon
+                const Icon(
+                  Icons.medication,
+                  color: Color.fromARGB(255, 82, 173, 248),
+                  size: 150,
+                ),
+
+                const SizedBox(height: 50),
+
+                // Patient Welcome Back Message
+                Text(
+                  'Welcome to Utibu Mobile!',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 82, 173, 248),
+                    fontSize: 20,
+                  ),
+                ),
+
+                const SizedBox(height: 50),
+
+                // email textfield
+                CustomTextField(
+                  controller: emailController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+
+                const SizedBox(height: 50),
+
+                // password textfield
+                CustomTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+
+                // sign in button
+
+                // forgot password option
+
+                // Register user option
+              ],
+            ),
+          ),
+        ));
+  }
+}
