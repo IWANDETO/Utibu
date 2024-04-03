577ef4921468c378580eb28c4e4e131cd792645e
IWANDETO <wandettoian@gmail.com>
2024-04-03 15:51:35 +0300

    Modified Main App, created login page

--- utibu_mobile/lib/main.dart
+++ utibu_mobile/lib/main.dart
@@ -1,37 +1,18 @@
 import 'package:flutter/material.dart'; 
+import 'pages/login.dart'
 
-void main() => runApp(MyApp()); 
+void main() {
+  runApp(const MyApp()); 
+} 
 
 class MyApp extends StatelessWidget { 
-  // This widget is the root of your application. 
+  const MyApp({super.key});
+
   @override 
   Widget build(BuildContext context) { 
     return MaterialApp( 
-      title: 'Utibu Mobile Application', 
-      theme: ThemeData( 
-        primarySwatch: Colors.blue, 
-      ), 
-      home: MyHomePage(title: 'Home page'), 
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(), 
     ); 
   }
 } 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({required Key key, this.title}) : super(key: key); 
-
-  final String title; 
-  
-  @override 
-  Widget build(BuildContext context) { 
-    return Scaffold( 
-      appBar: AppBar( 
-        title: Text(this.title), 
-      ), 
-      body: Center( 
-        child: 
-            Text( 
-              'Welcome to Utibu App', 
-            ) 
-      ), 
-    ); 
-  }
-}
